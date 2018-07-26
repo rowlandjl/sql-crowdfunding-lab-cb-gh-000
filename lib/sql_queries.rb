14f8cdc900c9565d4c8defa4251bf74973957248
Jessica Rowland <rowlandjl@vcu.edu>
2018-07-26 00:48:33 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -15,7 +15,11 @@ def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-"Write your SQL query Here"
+"select title, (sum(amount) - funding_goal) as over
+from projects p
+left outer join pledges p2 on p.id = p2.project_id
+group by title
+having over >= 0"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
