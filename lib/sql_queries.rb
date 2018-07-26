22efa20baf75cf9bf2607ca3abe3dc38a7a71f95
Jessica Rowland <rowlandjl@vcu.edu>
2018-07-26 00:42:59 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -27,5 +27,5 @@ def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_ca
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
-"select category, sum(amount) from projects p left join pledges pw on p.id = p2.project_id where category = 'books'"
+"select category, sum(amount) from projects p left join pledges p2 on p.id = p2.project_id where category = 'books'"
 end
